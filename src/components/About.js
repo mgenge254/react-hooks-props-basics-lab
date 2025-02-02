5ef74832e4e57e76646f9ca642525948a5ee0f11
Newton Manyisa <manyisanewton26@gmail.com>
2025-02-03 00:36:07 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
-
-function About() {
+import Links from "./Links";
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* <p>Put the bio in here</p>
+      implementing conditional renedring using ternary operator where
+      if(props.bio && props.bio.lenth>1 //that is if the bio has content{
+        therefore display props.bio else display null
+      }) */}
+       {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
+      
+     
     </div>
   );
 }
